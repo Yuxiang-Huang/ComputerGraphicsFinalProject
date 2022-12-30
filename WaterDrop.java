7e5813b94d269eec0739306cacb7366b32c5b82c
Yuxiang Huang <yhuang30@stuy.edu>
2022-12-30 13:46:31 -0500

    testing waterdrop

--- WaterDrop.java
+++ WaterDrop.java
@@ -9,17 +9,7 @@ public interface WaterDrop {
 
         PolygonMatrix polys = new PolygonMatrix();
 
-        polys.addCylinder(250, 100, 450, 0, 100, 20);
-
-        Matrix transform = new Matrix();
-        transform.ident();
-        Stack<Matrix> csystems = new Stack<Matrix>();
-        Matrix tmp = new Matrix(Matrix.ROTATE, Math.PI/4 ,'X');
-        tmp.mult(transform);
-        csystems.push(tmp.copy());
-
-        polys.mult(csystems.peek());
-
+        polys.addCurve(200, 200, 200, 490, -200, 0, 200, 430, 0.01, Matrix.HERMITE, 20);
         polys.drawPolygons(s);
         
         s.display();
