8510fc3f70e23e8c809fe72f181230e4657bd51b
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-03 21:00:24 -0500

    add z component to three body motion and ready for experimenting for good values

--- Body.java
+++ Body.java
@@ -22,11 +22,7 @@ public class Body{
   
     public Body (double mass, int type){
       m = mass;
-      dx = Math.random() * initialV * 2 - initialV;
-      dy = Math.random() * initialV * 2 - initialV;
-      //dz = Math.random() * initialV * 2 - initialV;
-  
-      double radius = Math.random() * 50 + 100;
+      double radius = Math.random() * 50 + 150;
       double angle = 0;
   
       if (type == 0){
@@ -52,11 +48,23 @@ public class Body{
   
       x = Math.cos(angle) * radius + Screen.XRES/2;
       y = Math.sin(angle) * radius + Screen.YRES/2;
-    //   z = Math.random() * 150 + 100;
+      z = Math.random() * 100 + 100;
+      z = 150;
+
+      dx = Math.random() * initialV;
+      if (x > Screen.XRES/2){
+        dx *= -1;
+      }
+      dy = Math.random() * initialV;
+      if (y > Screen.YRES/2){
+        dy *= -1;
+      }
+      dz = Math.random() * initialV * 2 - initialV;
     }
   
     public double dist(Body other){
-        return Math.sqrt(Math.abs(x - other.x) * Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(y - other.y));
+        return Math.sqrt(Math.abs(x - other.x) * Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(y - other.y)
+        + Math.abs(z - other.z) * Math.abs(z - other.z));
       }
   
     public void attract (Body other, double t){
@@ -64,16 +72,20 @@ public class Body{
         double a = G * other.m / d / d;
 
         double theta = Math.atan2(other.y - y, other.x - x);
-        double ax = a * Math.cos(theta);
-        double ay = a * Math.sin(theta);
+        double phi = Math.acos((other.z - z) / d);
+
+        double ax = a * Math.cos(theta) * Math.sin(phi);
+        double ay = a * Math.sin(theta) * Math.sin(phi);
+        double az = a * Math.cos(phi);
 
         //System.out.println("x: " + x + ", y: " + y);
 
         x += dx * t + 0.5 * ax * t * t;
         y += dy * t + 0.5 * ay * t * t;
+        y += dz * t + 0.5 * az * t * t;
     
         dx += ax * t;
         dy += ay * t;
-        // dz += (other.z - z) / d / d * G * m * other.m;
+        dz += az * t;
     }
   }
--- ThreeBody.java
+++ ThreeBody.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
 import javax.swing.*;
+import javax.swing.plaf.PanelUI;
 import javax.imageio.stream.*;
 import java.net.URL;
 
@@ -12,30 +13,53 @@ public class ThreeBody{
   public static void main(String[] args) throws Exception {
     Screen s = new Screen();
 
-    int total = 30;
+    int total = 60;
 
-    Body b0 = new Body(10 * 10E11 * 1, 0);
-    Body b1 = new Body(11* 10E11 * 1, 1);
-    Body b2 = new Body(12 * 10E11 * 1, 2);
+    Body b0 = new Body(15 * 10E11 * 1, 0);
+    Body b1 = new Body(17.5* 10E11 * 1, 1);
+    Body b2 = new Body(20 * 10E11 * 1, 2);
     Body planet = new Body(10E7, 3);
 
     b2.dx = - b0.dx - b1.dx;
     b2.dy = - b0.dy - b1.dy;
+    b2.dz = - b0.dz - b1.dz;
 
-    int factor = 300;
+    b0.x = 567.0066478886442;
+    b0.y = 505.2412746706353;
+    b0.z = 150.0;
+    b0.dx = -2.8573114397517454;
+    b0.dy = -3.567840443430392;
+    b0.dz = -3.547111752616753;
+    
+    b1.x = 245.57444101829284;
+    b1.y = 411.3078019482365;
+    b1.z = 150.0;
+    b1.dx = 4.77430043796929;
+    b1.dy = -2.8682049162909444;
+    b1.dz = 2.41437157747324;
+    
+    b2.x = 398.94815576217;
+    b2.y = 204.35944267761758;
+    b2.z = 150.0;
+    b2.dx = -1.916988998217545;
+    b2.dy = 6.436045359721336;
+    b2.dz = 1.132740175143513;
 
-    b0.x = -0.6 * factor + Screen.XRES/2;
-    b0.y = -0.8 * factor + Screen.YRES/2;;
+    //int factor = 300;
 
-    b1.x = 0.3 * factor + Screen.XRES/2;;
-    b1.y = 0.6 * factor + Screen.YRES/2;;
+    // b0.x = -0.6 * factor + Screen.XRES/2;
+    // b0.y = -0.8 * factor + Screen.YRES/2;;
 
-    b2.x = -0.3 * factor + Screen.XRES/2;;
-    b2.y = 0.5 * factor + Screen.YRES/2;;
+    // b1.x = 0.3 * factor + Screen.XRES/2;;
+    // b1.y = 0.6 * factor + Screen.YRES/2;;
 
-    // bodyInfo(b0, 0);
-    // bodyInfo(b1, 1);
-    // bodyInfo(b2, 2);
+    // b2.x = -0.3 * factor + Screen.XRES/2;;
+    // b2.y = 0.5 * factor + Screen.YRES/2;;
+
+    bodyInfo(b0, 0);
+    bodyInfo(b1, 1);
+    bodyInfo(b2, 2);
+    bodyInfo(planet);
 
     BufferedImage firstImage = s.getimg();
 
@@ -91,10 +115,10 @@ public class ThreeBody{
 
     PolygonMatrix polys = new PolygonMatrix();
 
-    polys.addSphere(b0.x, b0.y, b0.z, 10, 20); //* b0.z / 25
-    polys.addSphere(b1.x, b1.y, b1.z, 13, 20); 
-    polys.addSphere(b2.x, b2.y, b2.z, 15, 20);
-    polys.addSphere(planet.x, planet.y, planet.z, 5, 20);
+    polys.addSphere(b0.x, b0.y, b0.z, 15 * b0.z / 150, 20);
+    polys.addSphere(b1.x, b1.y, b1.z, 17.5 * b1.z / 150, 20); 
+    polys.addSphere(b2.x, b2.y, b2.z, 20 * b2.z / 150 , 20);
+    polys.addSphere(planet.x, planet.y, planet.z, 7 * planet.z / 150, 20);
 
     polys.drawPolygons(s);
   }
@@ -106,6 +130,16 @@ public class ThreeBody{
     System.out.println("b" + i + ".dx = " + b.dx + ";");
     System.out.println("b" + i + ".dy = " + b.dy + ";");
     System.out.println("b" + i + ".dz = " + b.dz + ";");
+    System.out.println();
+  }
+
+  public static void bodyInfo(Body b){
+    System.out.println("planet" + ".x = " + b.x + ";");
+    System.out.println("planet" + ".y = " + b.y + ";");
+    System.out.println("planet" + ".z = " + b.z + ";");
+    System.out.println("planet" + ".dx = " + b.dx + ";");
+    System.out.println("planet" + ".dy = " + b.dy + ";");
+    System.out.println("planet" + ".dz = " + b.dz + ";");
     System.out.println();
   }
 }
