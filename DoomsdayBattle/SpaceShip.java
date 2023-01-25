39c8ce3a00e33b20eb4654afc714a71b1f980c67
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-24 19:20:21 -0500

    rename directories

---
+++ DoomsdayBattle/SpaceShip.java
@@ -0,0 +1,152 @@
+import java.util.*;
+
+public class SpaceShip {
+    public double x, y, z;
+    public double xtheta = -Math.PI / 6;
+    public double ztheta = Math.PI / 2;
+    int size = 15;
+
+    //for special movement
+    double randomDirection = Math.random() * Math.PI * 2;
+
+    //for explosion
+    public boolean expand = true;
+    double scale = 1;
+
+    public SpaceShip(int i, int j){
+        x = Screen.XRES * (2 * i + 3) / 12;
+        y = Screen.YRES * (2 * j + 3) / 14;
+    }
+
+    public void forward (){
+        x += Screen.YRES / 12 / 25;
+    }
+
+    public void random (){
+        //randomly escape
+        move(randomDirection);
+    }
+
+    public void escape(WaterDrop sfp){
+        //rotate toward runaway
+        double direction = Math.atan2(y - sfp.y, x - sfp.x);
+        move(direction);
+    }
+
+    public void move(double direction){
+        //stats
+        int speed = 3;
+        double rotateSpeed = Math.PI / 50;
+
+        ztheta -= Math.PI/2;
+
+        //rotate toward direction
+        if (Math.abs(direction - ztheta) < rotateSpeed){
+            ztheta = direction; 
+            speed *= 1.5;
+        } else if (Math.abs(direction - ztheta) < Math.PI){
+            ztheta += Math.abs(direction - ztheta) / (direction - ztheta) * rotateSpeed;
+        } else{
+            ztheta -= Math.abs(direction - ztheta) / (direction - ztheta) * rotateSpeed;
+        }
+
+        //move
+        x += Math.cos(direction) * speed;
+        y += Math.sin(direction) * speed;
+
+        ztheta += Math.PI/2;
+    }
+
+    public void display(Screen s){
+        //translate to the center of the ship
+        Matrix transform = new Matrix();
+        transform.ident();
+        Stack<Matrix> csystems = new Stack<Matrix>();
+        Matrix tmp = new Matrix(Matrix.TRANSLATE, x, y, z);
+        tmp.mult(transform);
+        csystems.push(tmp.copy());
+
+        //rotate
+        tmp = new Matrix(Matrix.ROTATE, ztheta - Math.PI / 2, 'Z');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        tmp = new Matrix(Matrix.ROTATE, xtheta, 'X');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        tmp = new Matrix(Matrix.ROTATE, Math.PI/2, 'Y');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        tmp = new Matrix(Matrix.ROTATE, Math.PI / 2, 'Z');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        //body
+        PolygonMatrix polys = new PolygonMatrix();
+        polys.addSphere(0, 0, 0, size, 20);
+        polys.addTorus(0, 0, 0, size/2, size * 2, 20);
+
+        //draw
+        polys.mult(csystems.peek());
+        polys.drawPolygons(s);
+
+        //other parts
+        polys = new PolygonMatrix();
+        polys.addCylinder(0, size, size * 3, 0, size/4, 20);
+        polys.addCone(0, size * 2, size * 4.5, 0, size/3, 20);
+        //back
+        polys.addCylinder(size, -size/3, -size*4, 0, size/3, 20);
+        polys.addCylinder(-size, -size/3, -size*4, 0, size/3, 20);
+        polys.addCylinder(0, -size/3, -size*3.5, 0, size/3, 20);
+        //front
+        polys.addCylinder(size/1.5, size, size*3.5, 0, size/4, 20);
+        polys.addCylinder(-size/1.5, size, size*3.5, 0, size/4, 20);
+
+        //rotate
+        tmp = new Matrix(Matrix.ROTATE, Math.PI/2, 'X');
+        tmp.mult(csystems.peek());
+        csystems.push(tmp.copy());
+
+        //draw
+        polys.mult(csystems.peek());
+        polys.drawPolygons(s);
+        csystems.pop();
+    }
+
+    public void explode(Screen s, ArrayList<SpaceShip> explode){
+         //translate to the center of the ship
+         Matrix transform = new Matrix();
+         transform.ident();
+         Stack<Matrix> csystems = new Stack<Matrix>();
+         Matrix tmp = new Matrix(Matrix.TRANSLATE, x, y, z);
+         tmp.mult(transform);
+         csystems.push(tmp.copy());
+
+        PolygonMatrix polys = new PolygonMatrix();
+        polys.addSphere(0, 0, 0, scale * size * 2, 20);
+
+        //expand or contract
+        if (expand){
+            scale += 0.2;
+            if (scale >= 2.5){
+                expand = false; 
+            }
+        } else{
+            scale -= 0.1;
+            if (scale <= 0){
+                explode.remove(this);
+            }
+        }
+ 
+         //draw
+         polys.mult(csystems.peek());
+         polys.drawPolygons(s);
+         csystems.pop();
+    }
+}
---
+++ DoomsdayBattle/WaterDrop.java
@@ -0,0 +1,278 @@
+import java.util.*;
+import java.awt.*;
+
+public class WaterDrop {
+    public double x, y, z;
+    int size = 50;
+    double theta = 0;
+    public boolean acc = false;
+    public boolean intro = true;
+
+    double direction = 0;
+    double rotateSpeed = Math.PI * 2 / 100;
+
+    //for expanding
+    double expand = 0;
+    double expandX = 0;
+    double expandY = 0;
+    double expandAngle = 0;
+
+    //special movement
+    boolean lock0 = true;
+    boolean lock1 = true;
+
+    public WaterDrop(){
+        x = Screen.XRES/2;
+        y = Screen.YRES;
+        z = 0;
+    }
+
+    public void update(ArrayList<SpaceShip> ships, ArrayList<SpaceShip> explode){
+        int speed = 25;
+        while (speed > 0){
+            //no more targets
+            if (ships.size() == 0){
+                break;
+            } 
+            //special movements
+            if (ships.size() == 15 && lock0){
+                //end of first col
+                direction = - Math.PI/2;
+                double distance = y;
+                if (distance < speed){
+                    y = 0;
+                    speed -= distance;
+                    lock0 = false;
+                } else{
+                    x = speed * Math.cos(direction) + x;
+                    y = speed * Math.sin(direction) + y;
+                    speed = 0;
+                }
+            }
+            else if (ships.size() == 10 && lock1){
+                //end of second col
+                direction = Math.PI/2;
+                double distance = Screen.YRES - y;
+                if (distance < speed){
+                    y = Screen.YRES;
+                    speed -= distance;
+                    lock1 = false;
+                } else{
+                    x = speed * Math.cos(direction) + x;
+                    y = speed * Math.sin(direction) + y;
+                    speed = 0;
+                }
+            } else{
+                SpaceShip target = ships.get(0);
+                double distance = dist(target, this);
+                //find closest ship
+                for (SpaceShip ship : ships){
+                    if (dist(ship, this) < distance){
+                        distance = dist(ship, this);
+                        target = ship;
+                    }
+                    //ship responses
+                    if (ships.size() <= 5){
+                        ship.escape(this);
+                    } else if (ships.size() <= 10){
+                        ship.random();
+                    } else if (ships.size() > 15){
+                        ship.forward();
+                    }
+                }
+
+                //direction
+                direction = Math.atan2(target.y - y, target.x - x);
+
+                //move toward it
+                if (distance < speed){
+                    //destroy
+                    x = target.x;
+                    y = target.y;
+                    explode.add(target);
+                    ships.remove(target);
+                    speed -= distance;
+                } else{
+                    //not enough speed
+                    x = speed * Math.cos(direction) + x;
+                    y = speed * Math.sin(direction) + y;
+                    speed = 0;
+                }
+
+                //?
+                direction += Math.PI/2;
+            }
+        }
+    }
+
+    public void end(){
+        int speed = 25;
+        while (speed > 0){
+            //direction
+            double targetx = Screen.XRES/2;
+            double targety = Screen.YRES/2;
+            direction = Math.atan2(targety - y, targetx - x);
+            double distance = Math.sqrt((x - targetx) * (x - targetx) + (y - targety) * (y - targety));
+            //move toward it
+            if (distance < speed){
+                x = targetx;
+                y = targety;
+                speed = 0;
+            } else{
+                //not enough speed
+                x = speed * Math.cos(direction) + x;
+                y = speed * Math.sin(direction) + y;
+                speed = 0;
+            }
+            direction += Math.PI/2;
+        }
+    }
+
+    public static double dist (SpaceShip ship, WaterDrop sfp){
+        return Math.sqrt((sfp.x - ship.x) * (sfp.x - ship.x) + (sfp.y - ship.y) * (sfp.y - ship.y));
+    }
+
+    public void display(Screen s){
+        //translate to the center of the ship
+        Matrix transform = new Matrix();
+        transform.ident();
+        Stack<Matrix> csystems = new Stack<Matrix>();
+        Matrix tmp = new Matrix(Matrix.TRANSLATE, x, y, z);
+        tmp.mult(transform);
+        csystems.push(tmp.copy());
+
+        //direction
+        tmp = new Matrix(Matrix.ROTATE, direction, 'Z');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        //self rotate
+        theta += rotateSpeed;
+        tmp = new Matrix(Matrix.ROTATE, theta, 'Y');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        //dilate
+        if (intro){
+            tmp = new Matrix(Matrix.SCALE, 5, 5, 5);
+            tmp.mult(csystems.peek());
+            csystems.push(tmp.copy());
+        }
+
+        //draw
+        PolygonMatrix polys = new PolygonMatrix();
+        polys.addCurve(0, 0, 0, size, -size, 0, 0, size, 0, Matrix.HERMITE, 20);
+        polys.mult(csystems.peek());
+        polys.drawPolygons(s);
+
+        //accelearation animation
+        if (acc){
+            if (expand == 0){
+                expandX = x;
+                expandY = y;
+                expandAngle = direction + Math.PI/2;
+            }
+            animateAcc(s);
+            if (expand >= 2){
+                expand = 0;
+            } else{
+                expand += 0.4;
+            }
+        }
+    }
+
+    void animateAcc(Screen s){
+        direction += Math.PI/2;
+
+        Matrix transform = new Matrix();
+        transform.ident();
+        Stack<Matrix> csystems = new Stack<Matrix>();
+        Matrix tmp;
+
+        //translate to the center of the halo
+        if (intro){
+            tmp = new Matrix(Matrix.TRANSLATE, expandX + size * 5 * Math.cos(expandAngle), 
+            expandY + size * 5 * Math.sin(expandAngle), z);
+            tmp.mult(transform);
+            csystems.push(tmp.copy());
+        } else{
+            tmp = new Matrix(Matrix.TRANSLATE, expandX + size * Math.cos(expandAngle), 
+            expandY + size * Math.sin(expandAngle), z);
+            tmp.mult(transform);
+            csystems.push(tmp.copy());
+        }
+
+        //direction
+        tmp = new Matrix(Matrix.ROTATE, expandAngle - Math.PI/2, 'Z');
+        tmp.mult(csystems.peek());
+        csystems.pop();
+        csystems.push(tmp.copy());
+
+        //dilates
+        if (intro){
+            tmp = new Matrix(Matrix.SCALE, 5, 1, 5);
+            tmp.mult(csystems.peek());
+            csystems.push(tmp.copy());
+        }
+        tmp = new Matrix(Matrix.SCALE, expand, 1, 1);
+        tmp.mult(csystems.peek());
+        csystems.push(tmp.copy());
+
+        //draw
+        PolygonMatrix polys = new PolygonMatrix();
+        polys.addTorus(0, 0, z, 1, 5, 20);
+        polys.mult(csystems.peek());
+        polys.drawPolygons(s);
+
+
+        //lines
+        csystems = new Stack<Matrix>();
+
+        //translate to the tail
+        if (intro){
+            tmp = new Matrix(Matrix.TRANSLATE, x + size * 5 * Math.cos(direction), 
+            y + size * 5 * Math.sin(direction), z);
+            tmp.mult(transform);
+            csystems.push(tmp.copy());
+        } else{
+            tmp = new Matrix(Matrix.TRANSLATE, x + size * Math.cos(direction), 
+            y + size * Math.sin(direction), z);
+            tmp.mult(transform);
+            csystems.push(tmp.copy());
+        }
+
+        //dilates
+        if (intro){
+            tmp = new Matrix(Matrix.SCALE, 1, 5, 1);
+            tmp.mult(csystems.peek());
+            csystems.push(tmp.copy());
+        }
+
+        //draw
+        double b = 255;
+        double lastX = 0;
+        double lastY = 0;
+        for (int i = 0; i < 50; i ++){
+            EdgeMatrix edges = new EdgeMatrix();
+            edges.addEdge(lastX, lastY, 0, Math.cos(direction) + lastX, Math.sin(direction) + lastY, 0);
+            if (intro){
+                edges.addEdge(lastX-1, lastY, 0, lastX - 1, Math.sin(direction) + lastY, 0);
+                edges.addEdge(lastX+1, lastY, 0, lastX + 1, Math.sin(direction) + lastY, 0);
+            }
+            lastX += Math.cos(direction);
+            lastY += Math.sin(direction);
+
+            edges.mult(csystems.peek());
+            if (i == 0){
+                edges.drawEdges(s, new Color(255, 255, 255));
+            } else{
+                edges.drawEdges(s, new Color(0, 0, (int) b));
+                b -= 255.0 / 50;
+            }
+        }
+
+        direction -= Math.PI/2;
+    }
+}
