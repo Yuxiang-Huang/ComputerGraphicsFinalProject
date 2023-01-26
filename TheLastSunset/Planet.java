4b317160615fbad5908f67ab1a2880cfe7eec5cf
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-26 02:17:02 -0500

    Done with Venus

--- TheLastSunset/Planet.java
+++ TheLastSunset/Planet.java
@@ -20,6 +20,7 @@ public class Planet{
   double displaySize;
 
   ArrayList<Double> venusSnowflakes = new ArrayList<>();
+  ArrayList<Double> venusRandomAngle = new ArrayList<>();
 
   public Planet(String name, double size, double dist, double revTime, double selfRotateTime, int[][] rgb, double theta,
   ArrayList<Color> planet2D){
@@ -43,6 +44,8 @@ public class Planet{
       while (phi < Math.PI * 2 - Math.PI / 10){
         venusSnowflakes.add(phi);
         phi += Math.random() * Math.PI / 10 + Math.PI / 10;
+
+        venusRandomAngle.add(Math.random() * 2 * Math.PI);
       }
     }
   }
@@ -114,39 +117,40 @@ public class Planet{
 
   public void drawSnowFlakesVenus(Screen s, Stack<Matrix> csystems, int length, int limit){        
     //translate to the center of the planet
-    for (double phi : venusSnowflakes){
-        EdgeMatrix edges = new EdgeMatrix();
-        //translate to the center of this snowflake
-        double currLen = size * LastSunset.scale2D + length;
-        Matrix tmp = new Matrix(Matrix.TRANSLATE, currLen * Math.cos(phi), currLen * Math.sin(phi), 0);
-        tmp.mult(csystems.peek());
-        csystems.push(tmp.copy());
-        //rotate
-        tmp = new Matrix(Matrix.ROTATE, phi, 'Z');
-        tmp.mult(csystems.peek());
-        csystems.pop();
-        csystems.push(tmp.copy());
+    for (int i = 0; i < venusSnowflakes.size(); i ++){
+      double phi = venusSnowflakes.get(i);
+      EdgeMatrix edges = new EdgeMatrix();
+      //translate to the center of this snowflake
+      double currLen = size * LastSunset.scale2D + length;
+      Matrix tmp = new Matrix(Matrix.TRANSLATE, currLen * Math.cos(phi), currLen * Math.sin(phi), 0);
+      tmp.mult(csystems.peek());
+      csystems.push(tmp.copy());
+      //rotate
+      tmp = new Matrix(Matrix.ROTATE, venusRandomAngle.get(i), 'Z');
+      tmp.mult(csystems.peek());
+      csystems.pop();
+      csystems.push(tmp.copy());
 
-        //draw 6 edges
-        for (int j = 0; j < 6; j ++){
-            double alpha = Math.PI * 2 * j / 6;
-            edges.addEdge(0, 0, 0, length * Math.cos(alpha), length * Math.sin(alpha), 0);
-        }
-
-        //check matrix exclude the rotate by x
-        Matrix checkMatrix = new Matrix(Matrix.TRANSLATE, x, y, 0);
-        tmp = new Matrix(Matrix.TRANSLATE, currLen * Math.cos(phi), currLen * Math.sin(phi), 0);
-        tmp.mult(checkMatrix);
-        checkMatrix = tmp.copy();
-        //rotate
-        tmp = new Matrix(Matrix.ROTATE, phi, 'Z');
-        tmp.mult(checkMatrix);
-        checkMatrix = tmp.copy();
-
-        edges.drawEdgesCircularLimit(s, new Color (255, 255, 0), x, y, 
-        limit - (x + Screen.XRES/2), csystems.peek(), checkMatrix);
-        
-        csystems.pop();
+      //draw 6 edges
+      for (int j = 0; j < 6; j ++){
+          double alpha = Math.PI * 2 * j / 6;
+          edges.addEdge(0, 0, 0, length * Math.cos(alpha), length * Math.sin(alpha), 0);
       }
+
+      //check matrix exclude the rotate by x
+      Matrix checkMatrix = new Matrix(Matrix.TRANSLATE, x, y, 0);
+      tmp = new Matrix(Matrix.TRANSLATE, currLen * Math.cos(phi), currLen * Math.sin(phi), 0);
+      tmp.mult(checkMatrix);
+      checkMatrix = tmp.copy();
+      //rotate
+      tmp = new Matrix(Matrix.ROTATE, phi, 'Z');
+      tmp.mult(checkMatrix);
+      checkMatrix = tmp.copy();
+
+      edges.drawEdgesCircularLimit(s, new Color (255, 255, 0), x, y, 
+      limit - (x + Screen.XRES/2), csystems.peek(), checkMatrix);
+      
+      csystems.pop();
+    }
   }
 }
